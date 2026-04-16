c545f35af27894e2f3f51c5d625d1fce4dca0f94
“cwl” <1807682554@qq.com>
2026-04-16 20:33:51 +0800

    文章列表-筛选和分页功能

--- src/pages/Article/index.jsx
+++ src/pages/Article/index.jsx
@@ -21,8 +21,8 @@ const Article = () => {
     const articleStatusEnum = {
         1: <Tag color="warning">待审核</Tag>,
         2: <Tag color="success">审核通过</Tag>,
-        3: <Tag color="danger">审核不通过</Tag>,
-        4: <Tag color="default">草稿</Tag>,
+        // 3: <Tag color="danger">审核不通过</Tag>,
+        // 4: <Tag color="default">草稿</Tag>,
     }
 
     // 准备列数据
@@ -94,29 +94,52 @@ const Article = () => {
         }
     ]
 
-    //获取文章列表
+    //请求参数  TODO 显式瞅瞅可省略吧
+    const [reqParams, setReqParams] = useState({
+        status: '',       //文章状态
+        channel_id: '',   //频道id
+        begin_pubdate: '', //开始时间
+        end_pubdate: '', //结束时间
+        page: 1,        //页码
+        per_page: 10,   //每页条数
+    })
+
+    //1.初试获取文章列表数据
     const [articleList, setArticleList] = useState([])
-    const [count, setCount] = useState(0)//总条数
-    // useEffect(() => {
-    //     getArticleListAPI().then(res => {
-    //         setArticleList(res.data)
-    //     })
-    // }, [])
-    // useEffect(() => {
-    //     async function getArticleList() {
-    //         const res = await getArticleListAPI()
-    //         setArticleList(res.data)
-    //     }
-    //     getArticleList()
-    // }, [])
+    //文章总条数
+    const [count, setCount] = useState(0)
     useEffect(() => {
-        const getArticleList = async () => {
-            const res = await getArticleListAPI()
+        async function getArticleList() {
+        // const getArticleList = async () => {
+            const res = await getArticleListAPI(reqParams)
             setArticleList(res.data.results)
             setCount(res.data.total_count)
         }
         getArticleList()
-    }, [])
+    }, [reqParams])
+
+    //2.条件筛选获取文章列表数据
+    const onFinish = (formData) => {
+        console.log('formData:', +formData.date)
+        setReqParams({
+            ...reqParams,
+            channel_id: formData.channel_id,
+            status: formData.status,
+            begin_pubdate: formData.date[0].format('YYYY-MM-DD'),
+            end_pubdate: formData.date[1].format('YYYY-MM-DD')
+        })
+        //获取文章列表数据,触发useEffect
+    }
+
+    //分页改变
+    const onPageChange = (page) => {
+        setReqParams({ 
+            ...reqParams, 
+            page
+        })
+    }
+
+
     return (
         <div>
             <Card
@@ -128,7 +151,7 @@ const Article = () => {
                 }
                 style={{ marginBottom: 20 }}
             >
-                < Form initialValues={{ status: '' }}>
+                < Form initialValues={{ status: '' }} onFinish={onFinish}>
                     <Form.Item label="状态" name="status">
                         <Radio.Group>
                             <Radio value={''}>全部</Radio>
@@ -164,7 +187,11 @@ const Article = () => {
             </Card>
             {/* 表格数据,默认第一页数据 */}
             <Card title={`根据筛选条件共查询到 ${count} 条结果：`}>
-                <Table rowKey="id" columns={columns} dataSource={articleList} />
+                <Table rowKey="id" columns={columns} dataSource={articleList} pagination={{
+                    total: count, //总条数
+                    pageSize: reqParams.per_page, //每页条数
+                    onChange: onPageChange //分页改变
+                }}/>
             </Card>
         </div>
     )
